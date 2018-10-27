e44bdc9be82ebb62c71bf66148be3e68f07d6733
pettercosta <pettersonromao@gmail.com>
2018-10-27 18:32:58 -0300

    nunca mais configurar

--- app/src/main/java/com/smartgreenhouse/alphagrow/IndicePHActivity.java
+++ app/src/main/java/com/smartgreenhouse/alphagrow/IndicePHActivity.java
@@ -1,13 +1,20 @@
 package com.smartgreenhouse.alphagrow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class IndicePHActivity extends AppCompatActivity {
 
+    TextView tvUmidade;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indice_ph);
+        Intent intent = getIntent();
+        String umidade = intent.getStringExtra("umidade");
+        tvUmidade = (TextView) findViewById(R.id.textViewMetricaUmidade);
+        tvUmidade.setText(umidade);
     }
 }
